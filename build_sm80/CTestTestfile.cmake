2f85dde70e226d927632b399297674df32323be3
Davide-F5-Marincione <marincione.1927757@studenti.uniroma1.it>
2026-03-22 18:03:48 +0100

    removed build

--- build_sm80/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /leonardo_work/IscrC_eff-SAM2/azirilli/cuda-cachemir/pycudafhe
-# Build directory: /leonardo_work/IscrC_eff-SAM2/azirilli/cuda-cachemir/pycudafhe/build_sm80
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
--- build_sm80/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /leonardo_work/IscrC_eff-SAM2/azirilli/cuda-cachemir/pycudafhe/tests
-# Build directory: /leonardo_work/IscrC_eff-SAM2/azirilli/cuda-cachemir/pycudafhe/build_sm80/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[test_basics]=] "/leonardo_work/IscrC_eff-SAM2/azirilli/cuda-cachemir/pycudafhe/build_sm80/bin/test_basics")
-set_tests_properties([=[test_basics]=] PROPERTIES  _BACKTRACE_TRIPLES "/leonardo_work/IscrC_eff-SAM2/azirilli/cuda-cachemir/pycudafhe/tests/CMakeLists.txt;17;add_test;/leonardo_work/IscrC_eff-SAM2/azirilli/cuda-cachemir/pycudafhe/tests/CMakeLists.txt;22;add_cachemir_test;/leonardo_work/IscrC_eff-SAM2/azirilli/cuda-cachemir/pycudafhe/tests/CMakeLists.txt;0;")
