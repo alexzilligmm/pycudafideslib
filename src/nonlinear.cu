09841f70cdf3a9e7a0776b6f8c8eea6cbf2c164b
Davide-F5-Marincione <marincione.1927757@studenti.uniroma1.it>
2026-03-23 14:34:53 +0100

    added comments to nonlinear functions

--- src/nonlinear.cu
+++ src/nonlinear.cu
@@ -31,6 +31,8 @@ static void dbg_val(const char* label, const CC& cc,
     std::cout << "]\n";
 }
 
+
+/// @brief SiLU in the Cachemir's implementation
 Ctx silu(LlamaInference& llama, const Ctx& x_in) {
     const CC& cc = llama.cc();
     auto fn = [](double x) { return x / (std::exp(-x) + 1.0); };
@@ -54,6 +56,9 @@ Ctx silu_expDim(LlamaInference& llama, const Ctx& x_in) {
     return cc->EvalMult(y, pt_mask);
 }
 
+/// @brief Softmax in the Cachemir's implementation
+/// @param temp Additional squarings such that the computed function is actually
+///             exp(x * 2^temp) instead of exp(x). To add, as the name suggests, a temperature effect.
 Ctx softmax(LlamaInference& llama, const Ctx& x_in,
              int target_level_after_btp, int temp) {
     const CC& cc = llama.cc();
@@ -62,6 +67,9 @@ Ctx softmax(LlamaInference& llama, const Ctx& x_in,
 
     Ctx x = x_in;
 
+    // These guys are approximating exp(x) via the 
+    // canonical limit definition: exp(x) = lim_{n→∞} (1 + x/n)^n.
+    // They choose n=128=2^7, so they need 7 squarings to get the final result.
     Ptx pt_inv128 = const_pt(cc, 0.0078125, S, level_of(x));
     cc->EvalMultInPlace(x, pt_inv128);
     cc->RescaleInPlace(x);
@@ -75,23 +83,30 @@ Ctx softmax(LlamaInference& llama, const Ctx& x_in,
     }
 
     Ctx exp_ct = cc->EvalAdd(x, 0.0);
+    // TODO: why do they make a copy like so?
+    // can't they just
+    //   Ctx exp_ct = x; ???
 
     const double v = 8.0 / S;   // = 1/256 for S=2048
     cc->EvalMultInPlace(x, v);
     cc->RescaleInPlace(x);      // NL → 1
 
+    // Trick to compute total sum in log-time
     for (int j = 1; j < 256; j *= 2) {
         Ctx tmp = cc->EvalRotate(x, 1024 / j);
         cc->EvalAddInPlace(x, tmp);
     }
-
+    
     x = bootstrap_to(llama, x, (uint32_t)target_level_after_btp);
 
+    // TODO, understand: x right now is the total sum, right?
+    // if so then dnm should be 2 - x right now no?
     Ctx res = cc->EvalNegate(x);         // NL=2 (or auto-rescale from bootstrap NL=2)
     cc->RescaleInPlace(res);             // NL→1
     cc->EvalAddInPlace(res, 1.0);        // res = 1 - v*sum ≈ 0
     Ctx dnm = cc->EvalAdd(res, 1.0);     // dnm ≈ 1  (will converge to 1/(v*sum))
 
+    // TODO This just goldschmidt for 7 iterations, why not calling the primitive?
     for (int i = 0; i < 7; ++i) {
         cc->EvalSquareInPlace(res);
         cc->RescaleInPlace(res);
@@ -104,7 +119,9 @@ Ctx softmax(LlamaInference& llama, const Ctx& x_in,
 
     }
 
-    {
+    { // Rescaling exp_ct to match dnm's level, so we can multiply them together.
+      // TODO: Why the heck do we need to do this iteratively like this? Don't we have 
+      // a 'dropToLevel()' or something?
         uint32_t target = level_of(dnm);
         int iters = 0;
         while (level_of(exp_ct) < target) {
@@ -135,7 +152,7 @@ Ctx norm(LlamaInference& llama, const Ctx& x_in,
     const int hD = llama.size.hidDim;
     const auto& sk = llama.fhe->sk();
 
-    Ctx mean = cc->EvalAdd(x_in, 0.0);
+    Ctx mean = cc->EvalAdd(x_in, 0.0); // TODO: again making copy like so, wtf
     for (int i = S / hD; i < S; i *= 2) {
         Ctx tmp = cc->EvalRotate(mean, i);
         cc->EvalAddInPlace(mean, tmp);
@@ -145,9 +162,9 @@ Ctx norm(LlamaInference& llama, const Ctx& x_in,
     cc->RescaleInPlace(xd);
 
     drop_levels(cc, mean, 1);
-    Ctx varc = cc->EvalSub(xd, mean);
+    Ctx varc = cc->EvalSub(xd, mean); // so doing N*(X - mean)
 
-    cc->EvalSquareInPlace(varc);
+    cc->EvalSquareInPlace(varc); // it is variance so makes sense
     cc->RescaleInPlace(varc);
 
     for (int i = S / hD; i < S; i *= 2) {
@@ -156,9 +173,16 @@ Ctx norm(LlamaInference& llama, const Ctx& x_in,
     }
 
     double inv_d3 = 1.0 / std::pow((double)hD, 3.0);
-    cc->EvalMultInPlace(varc, inv_d3);
+    cc->EvalMultInPlace(varc, inv_d3); // 1/N^3 * sum(N^2*(X - mean)^2) = 1/N * sum((X - mean)^2) = variance
     cc->RescaleInPlace(varc);
+    
+    // --- Newton-Raphson for inverse square root ---
+    // TODO: Again a function without calling the primitive? Why?
+    // TODO: why is this not using the goldschmidt_inv_sqrt primitive if first place?
+    //      I would expect that both inv_sqrts are pretty much equal, both are second order methods...
 
+    // Do we know the range of varc? Otherwise this
+    // initialization of the guess could be fairly sketchy.
     Ctx ans = cc->EvalMult(varc, -42.1);
     cc->RescaleInPlace(ans);
     cc->EvalAddInPlace(ans, 7.37);
@@ -187,13 +211,14 @@ Ctx norm(LlamaInference& llama, const Ctx& x_in,
 
         ans = cc->EvalAdd(term1, term2);
     }
+    // --- End Newton-Raphson ---
 
     ans = bootstrap_to(llama, ans, (uint32_t)target_level_after_btp);
-
 
     Ctx varc_r = varc;
     reduce_to_level(cc, varc_r, level_of(ans), S);
 
+    // And now we are calling the primitive... eh?
     ans = goldschmidt_inv_sqrt(cc, S, varc_r, ans, 2);
 
     Ctx x_copy = x_in;
@@ -209,9 +234,9 @@ Ctx argmax(LlamaInference& llama, const Ctx& x_in) {
     const CC& cc = llama.cc();
     const int S  = llama.slots;
 
-    Ctx logit = softmax(llama, x_in, 14, 3);
+    Ctx logit = softmax(llama, x_in, 14, 3); // temp > 0 is needed to make the distribution more peaky
 
-    Ctx sum = cc->EvalAdd(logit, 0.0);
+    Ctx sum = cc->EvalAdd(logit, 0.0); // TODO: again with copying like so
     for (int j = 1; j < 256; j *= 2) {
         Ctx tmp = cc->EvalRotate(sum, 1024 / j);
         cc->EvalAddInPlace(sum, tmp);
@@ -219,6 +244,9 @@ Ctx argmax(LlamaInference& llama, const Ctx& x_in) {
 
     sum = bootstrap_to(llama, sum, 16);
 
+
+    // --- Goldschmidt for inverse ---
+    // TODO: WHYYYYY?! Why not just call the primitive?
     Ctx res = cc->EvalNegate(sum);
     cc->RescaleInPlace(res);       
     cc->EvalAddInPlace(res, 1.0);
@@ -233,12 +261,16 @@ Ctx argmax(LlamaInference& llama, const Ctx& x_in) {
         dnm = cc->EvalMult(dnm, tmp);
         cc->RescaleInPlace(dnm);
     }
+    // --- End Goldschmidt for inverse ---
 
-    Ctx logit_copy = cc->EvalAdd(logit, 0.0);
+    Ctx logit_copy = cc->EvalAdd(logit, 0.0); // TODO: i dunno what to say anymore.
     reduce_to_level(cc, logit_copy, level_of(dnm), S);
-    Ctx y = cc->EvalMult(logit_copy, dnm);
+    Ctx y = cc->EvalMult(logit_copy, dnm); // TODO: Soo we doing probabilities * (1/sum of probabilities) = probabilities, right?
     cc->RescaleInPlace(y);
 
+    // Actual argmax trick, if we have high enough temp
+    // then the softmax distribution with almost be one-hot encoding,
+    // and thus we can do this stuff to get the index of the max element.
     std::vector<double> idx_vec(S, 0.0);
     for (int i = 0; i < S; ++i)
         idx_vec[i] = (double)i;
